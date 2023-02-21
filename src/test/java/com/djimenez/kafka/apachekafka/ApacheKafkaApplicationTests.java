a98157a595ed7d39c26869c89fc57b1736588a99
diego jimenez <diegourieljd@gmail.com>
2023-02-21 10:45:03 -0600

    initial commit

---
+++ src/test/java/com/djimenez/kafka/apachekafka/ApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djimenez.kafka.apachekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
